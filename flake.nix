00a56d4dbd060bfcf4ce79b0f61c84564353e30d
Jack Cummings <jack@mudshark.org>
2022-11-22 21:36:50 -0800

    add formatter, expose go-maven-resolver as a package

--- flake.nix
+++ flake.nix
@@ -6,9 +6,14 @@
   inputs.devshell-flake.url = "github:numtide/devshell";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, flake-utils, devshell-flake, nixpkgs }:
-    let
-      overlay = final: prev: (rec {
+  outputs = {
+    self,
+    flake-utils,
+    devshell-flake,
+    nixpkgs,
+  }: let
+    overlay = final: prev:
+      rec {
         go-maven-resolver = prev.callPackage ./go-maven-resolver {};
 
         android2nix = {
@@ -17,46 +22,62 @@
           # For creating android2nix devshell
           mkDevshell = final.callPackage ./android2nix/mkDevshell.nix;
           # For loading android composition from devshell
-          loadAndroidComposition = devshell: final.androidenv.composeAndroidPackages
+          loadAndroidComposition = devshell:
+            final.androidenv.composeAndroidPackages
             (final.lib.importTOML devshell).android;
           # Android2nix gradle builder
           mkBuild = final.callPackage ./android2nix/mkBuild.nix;
           # Script for patching gradle source, so that it uses local maven repo
-          patch-maven-source = final.callPackage ./android2nix/patch-maven-source.nix { };
+          patch-maven-source = final.callPackage ./android2nix/patch-maven-source.nix {};
           # Android2nix generator for fetching dependencies
-          generate = prev.callPackage ./generate { inherit go-maven-resolver; };
+          generate = prev.callPackage ./generate {inherit go-maven-resolver;};
           # Builder for creating local maven repo
-          mkLocalMavenRepo = deps-path: final.callPackage ./android2nix/mkLocalMavenRepo.nix { inherit deps-path; };
+          mkLocalMavenRepo = deps-path: final.callPackage ./android2nix/mkLocalMavenRepo.nix {inherit deps-path;};
         };
-      })
+      }
       // devshell-flake.overlay final prev;
-    in
-      {
-        # Makes overlay available as an output
-        inherit overlay;
-        lib = {
-          # Helper function for creating flakes that build Android apps
-          mkAndroid2nixEnv = attrsetFn: flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (
-            system:
-              let
-                pkgs = import nixpkgs {
-                  inherit system;
-                  config.android_sdk.accept_license = true;
-                  config.allowUnfree = true;
-                  overlays = [ devshell-flake.overlay overlay ];
-                };
-              in
-                {
-                  inherit (pkgs.callPackage ./mkAndroid2nixEnv.nix (pkgs.callPackage attrsetFn {}))
-                    defaultPackage packages devShell
-                    ;
-                }
-          );
-        };
+  in {
+    # Makes overlay available as an output
+    inherit overlay;
+    lib = {
+      # Helper function for creating flakes that build Android apps
+      mkAndroid2nixEnv = attrsetFn:
+        flake-utils.lib.eachSystem ["x86_64-linux" "x86_64-darwin"] (
+          system: let
+            pkgs = import nixpkgs {
+              inherit system;
+              config.android_sdk.accept_license = true;
+              config.allowUnfree = true;
+              overlays = [devshell-flake.overlay overlay];
+            };
+          in {
+            inherit
+              (pkgs.callPackage ./mkAndroid2nixEnv.nix (pkgs.callPackage attrsetFn {}))
+              defaultPackage
+              packages
+              devShell
+              ;
+          }
+        );
+    };
 
-        defaultTemplate = {
-          path = ./template;
-          description = "Getting started template for Android2nix";
-        };
-      };
+    packages."x86_64-linux".go-maven-resolver =
+      (import nixpkgs {
+        system = "x86_64-linux";
+        overlays = [self.overlay];
+      })
+      .go-maven-resolver;
+
+    formatter."x86_64-linux" =
+      (import nixpkgs {
+        system = "x86_64-linux";
+        overlays = [self.overlay];
+      })
+      .alejandra;
+
+    defaultTemplate = {
+      path = ./template;
+      description = "Getting started template for Android2nix";
+    };
+  };
 }
