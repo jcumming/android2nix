66653b6be143b58f0e34e02e64ba663500007c95
Jack Cummings <jack@mudshark.org>
2022-11-28 21:19:45 -0800

    fmt, buildGo117Module -> buildGo119Module

--- go-maven-resolver/default.nix
+++ go-maven-resolver/default.nix
@@ -1,17 +1,20 @@
-{ lib, buildGo117Module, fetchFromGitHub }:
-
-let
+{
+  lib,
+  buildGo119Module,
+  fetchFromGitHub,
+}: let
   inherit (lib) strings;
-in buildGo117Module rec {
-  pname = "go-maven-resolver";
-  version = "v1.1.1";
+in
+  buildGo119Module rec {
+    pname = "go-maven-resolver";
+    version = "v1.1.2";
 
-  vendorSha256 = "sha256-dlqI+onfeo4tTwmHeq8heVKRzLU1gFEQ+4iv+8egN90=";
+    vendorSha256 = "sha256-dlqI+onfeo4tTwmHeq8heVKRzLU1gFEQ+4iv+8egN90=";
 
-  src = fetchFromGitHub rec {
-    owner = "Mazurel";
-    repo = pname;
-    rev = "7e4536d38b6e27c3cb52ba310e039df93b3b0229";
-    sha256 = "sha256-S7VyuRNyF+JepN0dN3hkZEsFIndNhwqO7u1fjXj5eFw=";
-  };
-}
+    src = fetchFromGitHub rec {
+      owner = "status-im";
+      repo = pname;
+      rev = "bd9da57e5ee35bbc79f20050dab14dad74afbd29";
+      sha256 = "sha256-1RWlvijJS+Tal6v72g4bGzfgglSbK2aTpS8Bzc846UE=";
+    };
+  }
